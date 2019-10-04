63dc826de0a6171dea4cc1eb876ca664fbb56446
oarcia <oarcia99@gmail.com>
2019-10-04 12:49:00 -0500

    actualizacion de rama segundo checkpoint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const morgan = require('morgan');//requerimos morgan
 const app = express();
 
-app.use(morgan('dev'));//aqui utilizamos morgan con el metodo dev 
+app.use(morgan('dev'));//aqui utilizamos morgan con el metodo dev para que nos mande por consola las peticiones http que se hacen
 //hay varios metodos de morgan pero ahorita utilizaremos el metodo dev para peticiones HTTP
 
 //utilizamos la ruta y desde la carpeta route requerimos router
